66d8b6742ce4460568266777bcb92b0ff3eaa5d8
Kabuto35 <bertrandmarine35122@gmail.com>
2025-04-05 14:40:57 +0200

    logo

--- front-page.php
+++ front-page.php
@@ -1,5 +1,11 @@
 <?php get_header() ?>
 
-
+    <header>
+        <ul>
+            <a href="" class="logo">
+                <img src="" alt="logo">
+            </a>
+        </ul>
+    </header>
 
 <?php get_footer() ?>
