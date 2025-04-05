6108aea79a3f5c5794c0fc66b0eb64b7cf12c708
Kabuto35 <bertrandmarine35122@gmail.com>
2025-04-05 13:56:02 +0200

    lapin

--- front-page.php
+++ front-page.php
@@ -2,6 +2,6 @@
 
 <header>prout j'aime les chiens et les chats</header>
 
-<main>hndcnv, et mon chats et les tortues</main>
+<main>hndcnv, et mon chats et les tortues et les lapins</main>
 
 <?php get_footer() ?>
