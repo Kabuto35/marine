e78e2b924bb28657c5dcb4f2800a0658985173b8
Kabuto35 <bertrandmarine35122@gmail.com>
2025-04-10 16:34:16 +0200

    update

--- front-page.php
+++ front-page.php
@@ -25,23 +25,88 @@
    <body>
     <div class="container_grotesquerie">
         <div></div>
-        <div></div>
+        <div>
+            <ul>
+                <li>
+                    <?php
+                    $video_path = "videos/ma_video.mp4";
+                    ?>
+
+                    <video width="640" height="360" controls>
+                        <source src="<?php echo $video_path; ?>" type="video/mp4">
+                        Votre navigateur ne supporte pas la vidéo HTML5.
+                    </video>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="container_monstre">
-        <div></div>
+        <div>
+            <ul>
+                <li>
+                    <?php
+                        $video_path = "videos/ma_video.mp4";
+                    ?>
+
+                    <video width="640" height="360" controls>
+                        <source src="<?php echo $video_path; ?>" type="video/mp4">
+                        Votre navigateur ne supporte pas la vidéo HTML5.
+                    </video>
+                </li>
+            </ul>
+        </div>
         <div></div>
     </div>
     <div class="container_american_horror_story">
         <div></div>
-        <div></div>
+        <div>
+            <ul>
+                <li>
+                    <?php
+                        $video_path = "videos/ma_video.mp4";
+                    ?>
+
+                    <video width="640" height="360" controls>
+                        <source src="<?php echo $video_path; ?>" type="video/mp4">
+                        Votre navigateur ne supporte pas la vidéo HTML5.
+                    </video>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="container_ratched">
         <div></div>
-        <div></div>
+        <div>
+            <ul>
+                <li>
+                    <?php
+                            $video_path = "videos/ma_video.mp4";
+                        ?>
+
+                        <video width="640" height="360" controls>
+                            <source src="<?php echo $video_path; ?>" type="video/mp4">
+                            Votre navigateur ne supporte pas la vidéo HTML5.
+                        </video>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="container_scream_queens">
         <div></div>
-        <div></div>
+        <div>
+            <ul>
+                <li>
+                    <?php
+                        $video_path = "videos/ma_video.mp4";
+                    ?>
+
+                    <video width="640" height="360" controls>
+                        <source src="<?php echo $video_path; ?>" type="video/mp4">
+                        Votre navigateur ne supporte pas la vidéo HTML5.
+                    </video>
+                </li>
+            </ul>
+        </div>
     </div>
    </body>
 
