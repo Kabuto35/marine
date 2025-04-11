92b52d92dea72f9badff6f6721e2ef740421cade
Kabuto35 <bertrandmarine35122@gmail.com>
2025-04-11 16:17:59 +0200

    font

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/minireset.min.css">
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
     <?php wp_head(); ?>
 
