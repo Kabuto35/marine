190e6867b07a0d45e662e07e23f6e96a184332a3
Kabuto35 <bertrandmarine35122@gmail.com>
2025-04-11 16:45:41 +0200

    mon lien

--- front-page.php
+++ front-page.php
@@ -15,11 +15,11 @@
                 </li>
             </ul>
             <ul class="menue">
-                <li><a href=""><?php echo get_field('grotesquerie'); ?></a></li>
-                <li><a href=""><?php echo get_field('monstre'); ?></a></li>
-                <li><a href=""><?php echo get_field('american_horror_stories'); ?></a></li>
-                <li><a href=""><?php echo get_field('ratched'); ?></a></li>
-                <li><a href=""><?php echo get_field('screem_queen'); ?></a></li>
+                <li><a href="" class="mon_lien"><?php echo get_field('grotesquerie'); ?></a></li>
+                <li><a href="" class="mon_lien_2"><?php echo get_field('monstre'); ?></a></li>
+                <li><a href="" class="mon_lien_3"><?php echo get_field('american_horror_stories'); ?></a></li>
+                <li><a href="" class="mon_lien_4"><?php echo get_field('ratched'); ?></a></li>
+                <li><a href="" class="mon_lien_5"><?php echo get_field('screem_queen'); ?></a></li>
             </ul>
         </ul>
      </nav>
