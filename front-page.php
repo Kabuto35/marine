caad13932b190eca3a1eb787b223f3b44c1ef0c5
Kabuto35 <bertrandmarine35122@gmail.com>
2025-05-12 16:50:43 +0200

    gedrg

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
    <header>
-     <nav>
+     <nav class="navbar">
             <div class="logo">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo MDLC">
             </div>
@@ -23,13 +23,48 @@
     <div>
         <h2><?php echo get_field('titre_bande_annonce'); ?></h2>
     </div>
-    <div>
-        <h2><?php echo get_field('presentation_titre'); ?></h2>
-        <div class="presentation"><?php echo nl2br(get_field('presentation_texte')); ?></div>
+
+    <h2><?php echo get_field('presentation_titre'); ?></h2>
+    <div class="presentation">
+        <h3><?php echo get_field('presentation_sous_titre'); ?></h3>
+        <div><?php echo nl2br(get_field('presentation_de_ryan')); ?></div>
+        <h3><?php echo get_field('presentation_sous_titre_2'); ?></h3>
+        <div><?php echo nl2br(get_field('presentation_des_oeuvres')); ?></div>
     </div>
-    <div>
-        <h2><?php echo get_field('galerie_titre'); ?></h2>
+
+    <h2><?php echo get_field('galerie_titre'); ?></h2>
+    <div class="galerie">
+        <div class="container">
+            <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/grotesquerie.jpg" alt="Logo MDLC" class="logo"></div>
+            <h3><?php echo get_field('grotesquerie_titre'); ?></h3>
+            <div class="séries"><?php echo nl2br(get_field('grotesquerie_description')); ?></div>
+        </div>
+        <div class="container">
+            <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/monstres.jpg" alt="Logo MDLC" class="logo"></div>
+            <h3><?php echo get_field('monstre_titre'); ?></h3>
+            <div class="séries"><?php echo nl2br(get_field('monstre_desciption')); ?></div>
+        </div>
+        <div class="container">
+            <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/5511004.webp" alt="Logo MDLC" class="logo"></div>
+            <h3><?php echo get_field('american_titre'); ?></h3>
+            <div class="séries"><?php echo nl2br(get_field('american_description')); ?></div>
+        </div>
+        <div class="container">
+             <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/screams.jpg" alt="Logo MDLC" class="logo"></div>
+            <h3><?php echo get_field('screem_titre'); ?></h3>
+            <div class="séries"><?php echo nl2br(get_field('screem_desciption')); ?></div>
+        </div>
         
     </div>
     </body>
+        <script>
+                window.addEventListener('scroll', function () {
+                const navbar = document.querySelector('.navbar');
+                if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+                } else {
+                navbar.classList.remove('scrolled');
+                }
+                });
+        </script>
 <?php get_footer() ?>
