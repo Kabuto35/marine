deccb811176c7cb0699be1c4c659aab1138eef6d
Kabuto35 <bertrandmarine35122@gmail.com>
2025-05-12 10:24:03 +0200

    fdgrt'ezr

--- front-page.php
+++ front-page.php
@@ -6,10 +6,9 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo MDLC">
             </div>
             <ul class="menue">
-                <li><a href="" class="mon_lien"><?php echo get_field('grotesquerie'); ?></a></li>
-                <li><a href="" class="mon_lien_1"><?php echo get_field('ryan'); ?></a></li>
-                <li><a href="" class="mon_lien_2"><?php echo get_field('monstre'); ?></a></li>
-                <li><a href="" class="mon_lien_3"><?php echo get_field('american_horror_stories'); ?></a></li>
+                <li><a href="" class="mon_lien"><?php echo get_field('bande-annonce'); ?></a></li>
+                <li><a href="" class="mon_lien_1"><?php echo get_field('presentation'); ?></a></li>
+                <li><a href="" class="mon_lien_2"><?php echo get_field('galerie'); ?></a></li>
             </ul>
         
      </nav>
@@ -21,21 +20,14 @@
    </header>
 
    <body>
-    <div class="grotesquerie">
-        <h2><?php echo nl2br(get_field('')); ?></h2>
+    <div>
+        <h2><?php echo get_field('titre_bande_annonce'); ?></h2>
     </div>
-    <div class="biographie">
-        <h2><?php echo nl2br(get_field('')); ?></h2>
-        <div>
-            <h3></h3>
-            <span><?php echo nl2br(get_field('')); ?></span>
-        </div>
+    <div>
+        <h2><?php echo get_field('presentation_titre'); ?></h2>
     </div>
-    <div class="monstre">
-        <h2><?php echo nl2br(get_field('')); ?></h2>
-    </div>
-    <div class="american_horror_stories">
-        <h2><?php echo nl2br(get_field('')); ?></h2>
+    <div>
+        <h2><?php echo get_field('galerie_titre'); ?></h2>
     </div>
     </body>
 <?php get_footer() ?>
