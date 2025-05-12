c50e6b7734cbd1afccc8656113f6e2ac766329cb
Kabuto35 <bertrandmarine35122@gmail.com>
2025-05-12 14:10:22 +0200

    esgez

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <?php wp_footer(); ?>
 
 <footer>
-    <h2><a href="" class="mon_lien"><?php echo get_field('titre_footer'); ?></h2>
     <div>
         <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('bande'); ?></a>
         <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('ryan'); ?></a>
         <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('galerie'); ?></a>
     </div>
+    <h2><a href="" class="titre_footer"><?php echo get_field('titre_footer'); ?></h2>
 </footer>
 
 
