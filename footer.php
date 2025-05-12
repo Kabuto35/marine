4f1fc2ca1b13c0ec984c6878c132013df65e6ce5
Kabuto35 <bertrandmarine35122@gmail.com>
2025-05-12 12:22:47 +0200

    vgj

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 <footer>
     <h2><a href="" class="mon_lien"><?php echo get_field('titre_footer'); ?></h2>
     <div>
-        <a href=""><a href="" class="mon_lien"><?php echo get_field('bande'); ?></a>
-        <a href=""><a href="" class="mon_lien"><?php echo get_field('ryan'); ?></a>
-        <a href=""><a href="" class="mon_lien"><?php echo get_field('galerie'); ?></a>
+        <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('bande'); ?></a>
+        <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('ryan'); ?></a>
+        <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('galerie'); ?></a>
     </div>
 </footer>
 
