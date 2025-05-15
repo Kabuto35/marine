cd7e62fbd9a9d57cdfd512e4f887c3bed114c812
Kabuto35 <bertrandmarine35122@gmail.com>
2025-05-15 16:21:40 +0200

    erh(ydfgthjyb,nb

--- footer.php
+++ footer.php
@@ -2,9 +2,11 @@
 
 <footer>
     <div>
-        <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('bande'); ?></a>
-        <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('ryan'); ?></a>
-        <a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('galerie'); ?></a>
+        <ul>
+            <li><a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('bande'); ?></a></li>
+            <li><a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('ryan'); ?></a></li>
+            <li><a class="mon_lien" href=""><a href="" class="mon_lien"><?php echo get_field('galerie'); ?></a></li>
+        </ul>        
     </div>
     <h2><a href="" class="titre_footer"><?php echo get_field('titre_footer'); ?></h2>
      <p class="copyright">&copy; 2025 Ryan Murphy Horror Productions. Tous droits réservés.</p>
